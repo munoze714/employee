be21e46b37f89424d5af85fe137b14cac71972a0
Elizabeth Munoz <munoz_714@live.com>
2020-06-25 18:17:34 -0700

    styling and component render

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 import API from "./utils/API";
+import Table from "../src/components/Table";
+import Jumbotron from "./components/Jumbotron";
 
 function App() {
   const [state, setState] = useState({
     employees: [],
     filteredEmployees: [],
+    sortedEmployees: [],
   });
 
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
   useEffect(() => {
     API.getEmployee()
       .then((res) => {
-        // console.log("this is our dataaa", res.data.results);
+        // console.log("data", res.data.results);
         var newEmps = [];
         var data = res.data.results;
 
@@ -21,15 +23,15 @@ function App() {
             email: data[i].email,
             phone: data[i].phone,
             pic: data[i].picture.thumbnail,
-            dob: data[i].dob.date,
+            location: data[i].location.city + ", " + data[i].location.state,
           };
           newEmps.push(emp);
         }
-        console.log("new data", newEmps);
+        // console.log("new data", newEmps);
         setState({ ...state, employees: newEmps });
       })
       .catch((err) => console.log(err));
-  });
+  }, []);
 
   var handleInputChange = (event) => {
     var newFiltered = [];
@@ -50,61 +52,33 @@ function App() {
     // console.log("these r the filted ppl", newFiltered);
     setState({ ...state, filteredEmployees: newFiltered });
   };
-
   // console.log(state);
   var empsToDisplay = state.employees;
 
   if (state.filteredEmployees.length > 0) {
     empsToDisplay = state.filteredEmployees;
   }
+  var handleSortChange = (event) => {
+    //   newSorted = [];
+    //   state.employees.forEach((empSort) => {
+    //     switch (ase) {
+    //       case value:
+    //         break;
+    //       default:
+    //         break;
+    //     }
+    //     empSort.sort((a, b) => a.date < b.date);
+    //   });
+  };
 
   return (
     <>
       <div className="App">
-        <div className="jumbotron  bg-dark text-white jumbotron-fluid">
-          <div className="container">
-            <h1 className="display-4">Employee Directory</h1>
-            <p className="lead"></p>
-
-            <div className="input-group">
-              <div className="input-group-prepend">
-                <span className="input-group-text">Search</span>
-              </div>
-              <input onChange={handleInputChange} placeholder="Name"></input>
-            </div>
-          </div>
-        </div>
-
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col"></th>
-              <th scope="col">First</th>
-              <th scope="col">Phone</th>
-              <th scope="col">Email</th>
-              <th scope="col">Date of Birth</th>
-            </tr>
-          </thead>
-          <tbody>
-            {empsToDisplay.map((emp, i) => {
-              return (
-                <tr key={i}>
-                  <td>
-                    <img src={emp.pic} alt="avatar"></img>
-                  </td>
-                  <td> {emp.name} </td>
-                  <td>
-                    <a href={"tel:+" + emp.phone}>{emp.phone}</a>
-                  </td>
-                  <td>
-                    <a href={"mailto:" + emp.email}>{emp.email}</a>
-                  </td>
-                  <td> {emp.dob} </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <Jumbotron handleInputChange={handleInputChange} />
+        <Table
+          empsToDisplay={empsToDisplay}
+          handleSortChange={handleSortChange}
+        />
       </div>
     </>
   );
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,9 +1,40 @@
 import React from "react";
-import "./style.css";
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
 function Table(props) {
-  return <table className="table"></table>;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col"></th>
+          <th scope="col" onClick={props.handleSortChange}>
+            Name
+          </th>
+          <th scope="col">Phone</th>
+          <th scope="col">Email</th>
+          <th scope="col">Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.empsToDisplay.map((emp, i) => {
+          return (
+            <tr key={i}>
+              <td>
+                <img src={emp.pic} alt="avatar"></img>
+              </td>
+              <td> {emp.name} </td>
+              <td>
+                <a href={"tel:+" + emp.phone}>{emp.phone}</a>
+              </td>
+              <td>
+                <a href={"mailto:" + emp.email}>{emp.email}</a>
+              </td>
+              <td> {emp.location} </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
